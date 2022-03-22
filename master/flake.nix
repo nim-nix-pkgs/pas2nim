cb8734bc8301c58bb35c19ab2d38a44fef9c3a1c
hugosenari <hugosenari@gmail.com>
2022-03-22 00:36:23 -0300

    chore: re-index ../nimpkgs/p/pas2nim

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-pas2nim-master.flake = false;
   inputs.src-pas2nim-master.owner = "nim-lang";
-  inputs.src-pas2nim-master.ref   = "refs/heads/master";
+  inputs.src-pas2nim-master.ref   = "master";
   inputs.src-pas2nim-master.repo  = "pas2nim";
   inputs.src-pas2nim-master.type  = "github";
   
